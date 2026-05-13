ca071d9e836f968987e200e733d97153268cf181
Nur <mn015014@gmail.com>
2026-05-13 03:24:00 +0300

    initial api

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,24 @@ app.get('/', (req, res) => {
 })
 
 // protected test route
-app.get('/api/protected', verifyToken, (req, res) => {
-  res.json({ message: 'Token valid', uid: req.user.uid })
-})
+// app.get('/api/protected', verifyToken, (req, res) => {
+//   res.json({ message: 'Token valid', uid: req.user.uid })
+// })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+//get all notes of specific user
+app.get('/notes', (req, res)=>{
+   res.status(200).json({ "userid": "user123", "title": "day 1", "content": "bla bla bla bla bla" })
+})
+
+//get all data of specific note
+app.get('/notes/:id',(req, res)=>{
+    const noteId = req.params.id
+    //get
+    res.status(200).json({ "userid": `user ${noteId}`, "title": `day ${noteId}`, "content": "bla bla bla bla bla bla bla" })
+})
+// http://localhost:3000
